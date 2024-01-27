3629116de0ea27837b724d888c7fd010e7a8489b
Martin Weyssow <martin.weyssow@umontreal.ca>
2024-01-26 20:51:58 -0500

    Update model pool and prompt templates.

--- prompt_templates.py
+++ prompt_templates.py
@@ -7,10 +7,19 @@ system_message = ("You are an expert programmer. Your task is to generate a code
                   "{code}\n"
                   "```")
 
-codellama_instruct_template = "[INST] {system_message}\n### Instruction:\n{instruction}\n[/INST]"
-
 templates = {
-    'codellama_instruct': codellama_instruct_template
+    'codellama-34b-instruct': "[INST] {system_message}\n### Instruction:\n{instruction}\n[/INST]",
+    'codellama-13b-instruct': "[INST] {system_message}\n### Instruction:\n{instruction}\n[/INST]",
+    'codellama-7b-instruct': "[INST] {system_message}\n### Instruction:\n{instruction}\n[/INST]",
+    'wizardcoder-33b': "{system_message}\n\n### Instruction:\n{instruction}\n\n### Response:",
+    'wizardcoder-15b': "{system_message}\n\n### Instruction:\n{instruction}\n\n### Response:",
+    'deepseek-coder-33b-instruct': "{system_message}\n\n### Instruction:\n{instruction}\n\n### Response:",
+    'deepseek-coder-6.7b-instruct': "{system_message}\n\n### Instruction:\n{instruction}\n\n### Response:",
+    'mistral-7b-instruct-code': "<|im_start|>{system_message}<|im_end|>\n<|im_start|>user\n{instruction}<|im_end|>\n<|im_start|>assistant",
+    'wizardlm-33b': "{system_message}\n\nUSER: {instruction}\nASSISTANT:",
+    'wizardlm-7b': "{system_message}\n\nUSER: {instruction}\nASSISTANT:",
+    'llama-2-13b-chat': "[INST] <<SYS>>\n{system_message}\n<</SYS>>\n{instruction}[/INST]",
+    'llama-2-70b-chat': "[INST] <<SYS>>\n{system_message}\n<</SYS>>\n{instruction}[/INST]",
 }
 
 
--- utils.py
+++ utils.py
@@ -6,8 +6,8 @@ import torch
 
 model_pool = [
     'gpt-4', 'gpt-3.5-turbo',
-    'codellama-34b-instruct', 'codellama-13b-instruct', 'wizardcoder-33b', 'wizardcoder-15b',
-    'starcoderplus', 'deepseek-coder-33b-instruct', 'deepseek-coder-6.7b-instruct', 'vicuna-33b-coder',
+    'codellama-34b-instruct', 'codellama-13b-instruct', 'codellama-7b-instruct',
+    'wizardcoder-33b', 'wizardcoder-15b', 'deepseek-coder-33b-instruct', 'deepseek-coder-6.7b-instruct',
     'mistral-7b-instruct', 'wizardlm-33b', 'wizardlm-7b', 'llama-2-13b-chat', 'llama-2-70b-chat'
 ]
 
